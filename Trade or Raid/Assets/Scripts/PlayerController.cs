dea85d42bbf4aec26f2bd3b6938a16dd172aaac6
Awesomecake <aaronodellbush@gmail.com>
2025-01-31 17:09:46 -0500

    added built-in color swapping for player instantiation

--- Trade or Raid/Assets/Scripts/PlayerController.cs
+++ Trade or Raid/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // 0,1,2 for Red, Green, Blue
+    [SerializeField] private int playerID;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite RedPlayerSprite;
+    [SerializeField] private Sprite GreenPlayerSprite;
+    [SerializeField] private Sprite BluePlayerSprite;
 
     [SerializeField] private float speed = 0.01f;
-    
+
     private bool isMoving = false;
     private Vector2 moveDirection = Vector2.zero;
+
+    private void Start()
+    {
+        UpdatePlayerColor();
+    }
 
     private void Update()
     {
@@ -27,4 +39,28 @@ public class PlayerController : MonoBehaviour
     {
         transform.Translate(moveDirection * speed);
     }
+
+    void UpdatePlayerColor()
+    {
+        switch (playerID%3)
+        {
+            case 1:
+                if (GreenPlayerSprite != null)
+                    spriteRenderer.sprite = GreenPlayerSprite;
+                break;
+            case 2:
+                if (BluePlayerSprite != null)
+                    spriteRenderer.sprite = BluePlayerSprite;
+                break;
+            default:
+                if (RedPlayerSprite != null)
+                    spriteRenderer.sprite = RedPlayerSprite;
+                break;
+        }
+    }
+
+
+#if UNITY_EDITOR
+    void OnValidate() { UnityEditor.EditorApplication.delayCall += UpdatePlayerColor; }
+#endif
 }
