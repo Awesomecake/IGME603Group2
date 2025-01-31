b5f71874d2760ef21e4ab2b233bdbbc1128e2e3e
MaxWhite2024 <mjwhite@scu.edu>
2025-01-31 16:53:20 -0500

    cleanup

--- Trade or Raid/Assets/Scripts/SpriteAnimations.cs
+++ Trade or Raid/Assets/Scripts/SpriteAnimations.cs
@@ -1,84 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class SpriteAnimations : MonoBehaviour
 {
     [Header("GameObject Variables")]
-    [SerializeField] private GameObject animationPivotObject;
-    [SerializeField] private SpriteRenderer spriteRenderer;
+    private SpriteRenderer spriteRenderer;
 
     [Header("Walk Animation Variables")]
-    [SerializeField] private float walkWobbleTimeLength = 0f;
-    [SerializeField] private float wobbleHorizontalStrength = 0f;
-    [SerializeField] private float wobbleVerticalStrength = 0f;
-    private bool isMoving = false;
+    [SerializeField] private float walkWobbleTimeLength = 1f;
+    [SerializeField] private float wobbleRotationStrength = 1f;
+    [SerializeField] private float walkVerticalStretchStrength = 1f;
+    [SerializeField] private float walkHorizontalSquashStrength = 1f;
+    [HideInInspector] public bool isMoving = false;
     private float startingScaleY = 0f;
     private float walkAnimProgress = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        //check sprite renderer of animationPivotObject
-        //if (spriteRenderer == null)
-        //    Debug.LogWarning("Please add a SpriteRenderer Component to " + animationPivotObject.name);
+        //get spriteRenderer
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
 
         //get startingScaleY
-        startingScaleY = animationPivotObject.transform.localScale.y;
+        startingScaleY = gameObject.transform.localScale.y;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Debug inputs REMOVE IF NOT DEBUGGING
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            BeginMovingLeft();
-        }
-        else if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
-        {
-            EndMoving();
-        }
+        //if(Input.GetKeyDown(KeyCode.LeftArrow))
+        //{
+        //    BeginMovingLeft();
+        //}
+        //else if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        //{
+        //    EndMoving();
+        //}
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            BeginMovingRight();
-        }
-        else if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
-        {
-            EndMoving();
-        }
+        //if (Input.GetKeyDown(KeyCode.RightArrow))
+        //{
+        //    BeginMovingRight();
+        //}
+        //else if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        //{
+        //    EndMoving();
+        //}
 
         //***** WALK ANIMATION *****
-        if(isMoving)
+        if (isMoving)
         {
             //increment walk animation progress
             walkAnimProgress += Time.deltaTime;
 
-            if(walkAnimProgress > 0f && walkAnimProgress <= walkWobbleTimeLength * 0.5f)
+            if(walkAnimProgress > 0f && walkAnimProgress <= walkWobbleTimeLength * 0.25f)
             {
                 //rotate sprite left
-                animationPivotObject.transform.Rotate(0f, 0f, wobbleHorizontalStrength * Time.deltaTime);
+                gameObject.transform.Rotate(0f, 0f, wobbleRotationStrength * Time.deltaTime);
 
-                //stretch sprite vertically and shrink sprite horizontally
-                //animationPivotObject.transform.localScale += new Vector3(-1, 1, 0) * Time.deltaTime;
+                StretchSpriteUp(Time.deltaTime);
             }
-            else if(walkAnimProgress > walkWobbleTimeLength * 0.5f && walkAnimProgress <= walkWobbleTimeLength)
+            else if(walkAnimProgress > walkWobbleTimeLength * 0.25f && walkAnimProgress <= walkWobbleTimeLength * 0.5f)
             {
                 //rotate sprite right
-                animationPivotObject.transform.Rotate(0f, 0f, -wobbleHorizontalStrength * Time.deltaTime);
+                gameObject.transform.Rotate(0f, 0f, -wobbleRotationStrength * Time.deltaTime);
 
-                //shrink sprite vertically and stretch sprite horizontally
-                //animationPivotObject.transform.localScale += new Vector3(1, -1, 0) * Time.deltaTime;
+                SquashSpriteDown(Time.deltaTime);
+            }
+            else if (walkAnimProgress > walkWobbleTimeLength * 0.5f && walkAnimProgress <= walkWobbleTimeLength * 0.75f)
+            {
+                //rotate sprite right
+                gameObject.transform.Rotate(0f, 0f, -wobbleRotationStrength * Time.deltaTime);
+
+                StretchSpriteUp(Time.deltaTime);
+            }
+            else if(walkAnimProgress > walkWobbleTimeLength * 0.75f && walkAnimProgress <= walkWobbleTimeLength)
+            {
+                //rotate sprite left
+                gameObject.transform.Rotate(0f, 0f, wobbleRotationStrength * Time.deltaTime);
+
+                SquashSpriteDown(Time.deltaTime);
             }
             else if(walkAnimProgress > walkWobbleTimeLength)
             {
                 //reset walkAnimProgress
                 walkAnimProgress = 0f;
+
+                Reset_Transform();
             }
-            //animationPivotObject.transform.localScale += new Vector3(0f, Mathf.Lerp(0f, startingScaleY * wobbleVerticalStrength,  Time.deltaTime * wobbleSpeed), 0f);
-            //animationPivotObject.transform.localScale += new Vector3(0f, Mathf.PingPong(Time.time * wobbleSpeed, startingScaleY * wobbleVerticalStrength), 0f);
-            //animationPivotObject.transform.localScale += ;
         }
     }
 
@@ -87,7 +99,7 @@ public class SpriteAnimations : MonoBehaviour
         //flip sprite to left
         spriteRenderer.flipX = false;
 
-        //
+        //Begin moving
         BeginMoving();
     }
 
@@ -96,7 +108,7 @@ public class SpriteAnimations : MonoBehaviour
         //flip sprite to right
         spriteRenderer.flipX = true;
 
-        //
+        //Begin moving
         BeginMoving();
     }
 
@@ -119,8 +131,20 @@ public class SpriteAnimations : MonoBehaviour
     }
     private void Reset_Transform()
     {
-        animationPivotObject.transform.localPosition = Vector3.zero;
-        animationPivotObject.transform.localRotation = Quaternion.identity;
-        animationPivotObject.transform.localScale = Vector3.one;
+        //reset all parts of the transform
+        gameObject.transform.localPosition = Vector3.zero;
+        gameObject.transform.localRotation = Quaternion.identity;
+        gameObject.transform.localScale = Vector3.one;
+    }
+    private void StretchSpriteUp(float deltaTime)
+    {
+        //stretch sprite vertically and shrink sprite horizontally
+        gameObject.transform.localScale += new Vector3(-walkHorizontalSquashStrength, walkVerticalStretchStrength, 0) * deltaTime;
+    }
+
+    private void SquashSpriteDown(float deltaTime)
+    {
+        //shrink sprite vertically and stretch sprite horizontally
+        gameObject.transform.localScale += new Vector3(walkHorizontalSquashStrength, -walkVerticalStretchStrength, 0) * deltaTime;
     }
 }
