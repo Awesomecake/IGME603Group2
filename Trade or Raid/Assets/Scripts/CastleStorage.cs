21732d0d6fa5c975f2eba283ebd45293875772f6
Awesomecake <aaronodellbush@gmail.com>
2025-02-03 17:25:32 -0500

    Added simple visual to storage

--- Trade or Raid/Assets/Scripts/CastleStorage.cs
+++ Trade or Raid/Assets/Scripts/CastleStorage.cs
@@ -6,12 +6,14 @@ public class CastleStorage : MonoBehaviour
 {
     [SerializeField] private int playerID = 0;
     public int wheatStored = 0;
+    [SerializeField] private List<GameObject> wheatStorageVisual;
 
     //Night Cycle
     public void DayPassed()
     {
         //remove wheat
         wheatStored--;
+        UpdateWheatVisual();
 
         if (wheatStored == 0 )
         {
@@ -24,10 +26,33 @@ public class CastleStorage : MonoBehaviour
         PlayerController playerController = collision.GetComponent<PlayerController>();
         if (playerController != null && playerID == playerController.PlayerID)
         {
-            //Add Wheat
-            wheatStored++;
+            if (wheatStored < 10)
+            {
+                //Add Wheat
+                wheatStored++;
 
-            Debug.Log("Player " + playerID + " Wheat Stored");
+                UpdateWheatVisual();
+
+                Debug.Log("Player " + playerID + " Wheat Stored");
+            }
+        }
+    }
+
+    public void UpdateWheatVisual()
+    {
+        for (int i = 0; i < wheatStorageVisual.Count; i++)
+        {
+            if (wheatStorageVisual[i] != null && i < 10)
+            {
+                if (i < wheatStored)
+                {
+                    wheatStorageVisual[i].active = true;
+                }
+                else
+                {
+                    wheatStorageVisual[i].active = false;
+                }
+            }
         }
     }
 }
